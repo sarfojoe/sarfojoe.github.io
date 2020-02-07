2e6578ff7ff649b4183903b4eae50e882a6ce4cb
ammarsaif <56692856+ammarsaif@users.noreply.github.com>
2020-02-07 14:20:40 +0200

    Updated

--- js/mail-script.js
+++ js/mail-script.js
@@ -1,6 +1,15 @@
     // -------   Mail Send ajax
 
-     $(document).ready(function() {
+    $(document).ready(function(){
+        setTimeout(function(){
+            $('.spinner').addClass('hide');
+            $('.site-wrap').removeClass('hide');
+        }, 3000);
+
+    });
+
+
+    $(document).ready(function(){
         var form = $('#myForm'); // contact form
         var submit = $('.submit-btn'); // submit button
         var alert = $('.alert-msg'); // alert div for show alert message
